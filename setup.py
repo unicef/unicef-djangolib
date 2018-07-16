2c239c7d9ce51c76e239d01d4101b9f521172452
Greg Reinbach <greg@reinbach.com>
2018-07-16 06:07:30 -0400

    Standard lib package, make use of pipenv

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ast
-import os
 import codecs
+import os.path
 import re
+import subprocess
 import sys
+from codecs import open
+from distutils import log
+from distutils.errors import DistutilsError
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 from setuptools.command.install import install
-from setuptools.command.test import test as TestCommand
+from setuptools.command.sdist import sdist as BaseSDistCommand
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-init = os.path.join(HERE, "src", "unicef_djangolib", "__init__.py")
-
+ROOT = os.path.realpath(os.path.dirname(__file__))
+init = os.path.join(ROOT, 'src', 'unicef_djangolib', '__init__.py')
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 _name_re = re.compile(r'NAME\s+=\s+(.*)')
+
+sys.path.insert(0, os.path.join(ROOT, 'src'))
 
 with open(init, 'rb') as f:
     content = f.read().decode('utf-8')
@@ -22,10 +28,34 @@ with open(init, 'rb') as f:
 
 
 def read(*files):
-    content = ''
+    content = []
     for f in files:
-        content += codecs.open(os.path.join(HERE, f), 'r').read()
-    return content
+        content.extend(codecs.open(os.path.join(ROOT, 'src', 'requirements', f), 'r').readlines())
+    return "\n".join(filter(lambda l:not l.startswith('-'), content))
+
+
+def check(cmd, filename):
+    out = subprocess.run(cmd, stdout=subprocess.PIPE)
+    f = os.path.join('src', 'requirements', filename)
+    reqs = codecs.open(os.path.join(ROOT, f), 'r').readlines()
+    existing = {re.split("(==|>=|<=>|<|)", name[:-1])[0] for name in reqs}
+    declared = {re.split("(==|>=|<=>|<|)", name)[0] for name in out.stdout.decode('utf8').split("\n") if name and not name.startswith('-')}
+
+    if existing != declared:
+        msg = """Requirements file not updated.
+Run 'make requiremets'
+""".format(' '.join(cmd), f)
+        raise DistutilsError(msg)
+
+class SDistCommand(BaseSDistCommand):
+
+    def run(self):
+        checks = {'install.pip': ['pipenv', 'lock', '--requirements'],
+                  'testing.pip': ['pipenv', 'lock', '-d', '--requirements']}
+
+        for filename, cmd in checks.items():
+            check (cmd, filename)
+        super().run()
 
 
 class VerifyTagVersion(install):
@@ -41,50 +71,30 @@ class VerifyTagVersion(install):
             sys.exit(info)
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ''
-
-    def run_tests(self):
-        import shlex
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
-
-tests_requires = read("src/requirements/test.txt")
-
-setup(
-    name=NAME,
-    version=VERSION,
-    url='https://github.com/unicef/unicef-djangolib',
-    author='UNICEF',
-    author_email='rapidpro@unicef.org',
-    description='',
-    long_description=read('README.rst'),
-    platforms=['any'],
-    license='Apache 2 License',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    include_package_data=True,
-    install_requires=read("src/requirements/install.txt"),
-    tests_requires=tests_requires,
-    extras_require={
-        'dev': tests_requires,
-    },
-    cmdclass={"verify": VerifyTagVersion,
-              'test': PyTest,
-              }
+setup(name=NAME,
+      version=VERSION,
+      url='https://github.com/unicef/unicef-djangolib',
+      author='UNICEF',
+      author_email='dev@unicef.org',
+      license="Apache 2 License",
+      description='Django package that handles exporting of data',
+      long_description=codecs.open('README.md').read(),
+      package_dir={'': 'src'},
+      packages=find_packages(where='src'),
+      include_package_data=True,
+      install_requires=read('install.pip'),
+      extras_require={
+          'test': read('install.pip', 'testing.pip'),
+      },
+      platforms=['any'],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Programming Language :: Python :: 3.6',
+          'Framework :: Django',
+          'Intended Audience :: Developers'],
+      scripts=[],
+      cmdclass={
+          'sdist': SDistCommand,
+          "verify": VerifyTagVersion,
+      }
 )
