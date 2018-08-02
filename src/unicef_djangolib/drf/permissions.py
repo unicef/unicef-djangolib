1cddeca3d539216316a4b903675b220a36be23b9
Domenico DiNicola <dom.dinicola@gmail.com>
2018-08-02 16:02:00 -0400

    remove rest functionality

--- src/unicef_djangolib/drf/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsSuperUser(BasePermission):
-    """Allows access only to super users."""
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_superuser
